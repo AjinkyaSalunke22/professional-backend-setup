48f31c9415167a99ed4507123e186bdefd934875
Ajinkya <ajinkya.salunke.official@gmail.com>
2024-11-26 15:28:29 +0530

    Util code files updated

--- src/app.js
+++ src/app.js
@@ -1,2 +1,25 @@
-ECHO is on.
-ECHO is on.
+import express, { urlencoded } from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import { LIMIT } from './constants'
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({
+    limit: LIMIT
+}))
+
+app.use(express.urlencoded({
+    extended: true, limit: LIMIT
+}))
+
+app.use(express.static("public"))
+
+app.use(express.cookieParser())
+
+export default app
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const DB_NAME = "backend-best-practices-demo"
+export const LIMIT = "16kb"
